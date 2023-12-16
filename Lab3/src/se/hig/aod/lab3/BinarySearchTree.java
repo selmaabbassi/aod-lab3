6d965660daaacc4833e6deea842159bf0d6a2f85
Selma Abbassi <selma.abbassi@lm.se>
2023-12-16 16:36:27 +0100

    Implemented BinarySearchTree and wrote tests

--- Lab3/src/se/hig/aod/lab3/BinarySearchTree.java
+++ Lab3/src/se/hig/aod/lab3/BinarySearchTree.java
@@ -1,5 +1,13 @@
 package se.hig.aod.lab3;
 
+/**
+ * A Binary search tree class that stores elements in a binary tree
+ * Elements in the tree are searchable
+ * Uses Generic type T that should extend {@link Comparable}
+ *
+ * @param <T> Generic type that extends {@link Comparable}
+ * @author Selma Abbassi
+ */
 public class BinarySearchTree<T extends Comparable<T>> implements SearchableDataStructure<T> {
 
     private TreeNode root;
@@ -17,24 +25,95 @@ public class BinarySearchTree<T extends Comparable<T>> implements SearchableData
 
     @Override
     public void addElement(T newElement) {
-
+        if (size == 0) {
+            this.root = new TreeNode(newElement, null, null);
+        } else {
+            this.root.addElementRecursive(newElement);
+        }
         size++;
     }
 
     @Override
     public T searchElement(T elementToFind) {
-        return null;
-    }
-
-    private void addElementRecursive(TreeNode current, T newElement) {
-        if (newElement.compareTo(current.element) < 0) {
-
+        if (size == 0) {
+            return null;
+        } else {
+            return this.root.searchElementRecursive(elementToFind);
         }
     }
 
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "";
+        } else {
+            return this.root.toStringRecursive("");
+        }
+    }
+
+    /**
+     * Inner class TreeNode that represents a node in the {@link BinarySearchTree}
+     * Stores the node's element of generic type T, and it's left and right child nodes
+     */
     public class TreeNode {
         T element;
         TreeNode left;
         TreeNode right;
+
+        public TreeNode(T element, TreeNode left, TreeNode right) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+        }
+
+        private void addElementRecursive(T newElement) {
+            if (newElement.compareTo(this.element) < 0) { //add element to left
+                if (this.left == null) { //left subtree empty
+                    this.left = new TreeNode(newElement, null, null);
+                } else { //keep going left
+                    this.left.addElementRecursive(newElement);
+                }
+            } else {
+                if (this.right == null) { //add element to right
+                    this.right = new TreeNode(newElement, null, null);
+                } else {
+                    this.right.addElementRecursive(newElement); //keep going right
+                }
+            }
+        }
+
+        private T searchElementRecursive(T elementToFind) {
+            if (this.element == elementToFind) {
+                return this.element;
+            } else {
+                if (elementToFind.compareTo(this.element) < 0) { //search element to left
+                    if (this.left == null) { //left subtree empty
+                        return null; //element cannot be found
+                    } else {
+                        return this.left.searchElementRecursive(elementToFind); //keep searching left
+                    }
+                } else {
+                    if (this.right == null) { //right subtree empty
+                        return null; //element cannot be found
+                    } else {
+                        return this.right.searchElementRecursive(elementToFind); //keep searching right
+                    }
+                }
+            }
+        }
+
+        private String toStringRecursive(String result) {
+            if (this.left != null) {
+                result = this.left.toStringRecursive(result);
+            }
+
+            result += this.element;
+
+            if (this.right != null) {
+                result = this.right.toStringRecursive(result);
+            }
+
+            return result;
+        }
     }
 }
--- Lab3/test/se/hig/aod/lab3/BinarySearchTreeTest.java
+++ Lab3/test/se/hig/aod/lab3/BinarySearchTreeTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+/**
+ * Test class for a {@link BinarySearchTree} with Integers
+ *
+ * @author Selma Abbassi
+ */
 class BinarySearchTreeTest {
 
     BinarySearchTree<Integer> searchTree;
@@ -16,12 +21,18 @@ class BinarySearchTreeTest {
         searchTree = new BinarySearchTree<>();
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#size()} should be 0 when empty
+     */
     @Test
     void testSizeOnEmptyTree() {
         int size = searchTree.size();
         assertEquals(0, size);
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#size()} should be 5 with initialized testData
+     */
     @Test
     void testSizeOnInitializedTree() {
         initializeSearchTree();
@@ -29,6 +40,9 @@ class BinarySearchTreeTest {
         assertEquals(5, size);
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#addElement(Comparable)} should add new element to empty tree and increase size
+     */
     @Test
     void testAddElementToEmptyTree() {
         searchTree.addElement(6);
@@ -36,6 +50,9 @@ class BinarySearchTreeTest {
         assertEquals(1, size);
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#addElement(Comparable)} should add new element to initialized tree and increase size
+     */
     @Test
     void testAddElementToInitializedTree() {
         initializeSearchTree();
@@ -44,6 +61,9 @@ class BinarySearchTreeTest {
         assertEquals(6, size);
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#searchElement(Comparable)} should return null when tree is empty
+     */
     @Test
     void testSearchElementInEmptyTree() {
         Integer searchElement = 3;
@@ -52,12 +72,59 @@ class BinarySearchTreeTest {
         assertNull(foundElement);
     }
 
+    /**
+     * Test method for {@link BinarySearchTree#searchElement(Comparable)} should find element to the left of root in initialized tree
+     */
     @Test
-    void testSearchElementInInitializedTree() {
+    void testSearchElementFoundToLeftInInitializedTree() {
+        initializeSearchTree();
+        int searchElement = 1;
+        Integer foundElement = searchTree.searchElement(searchElement);
+
+        assertEquals(searchElement, foundElement);
+    }
+
+    /**
+     * Test method for {@link BinarySearchTree#searchElement(Comparable)} should find element to the right of root in initialized tree
+     */
+    @Test
+    void testSearchElementFoundToRightInInitializedTree() {
+        initializeSearchTree();
         Integer searchElement = 3;
         Integer foundElement = searchTree.searchElement(searchElement);
 
         assertEquals(searchElement, foundElement);
+    }
+
+    /**
+     * Test method for {@link BinarySearchTree#searchElement(Comparable)} should return null when element not found in initialized tree
+     */
+    @Test
+    void testSearchElementNotFoundInInitializedTree() {
+        initializeSearchTree();
+        int searchElement = 7;
+        Integer foundElement = searchTree.searchElement(searchElement);
+
+        assertNull(foundElement);
+    }
+
+    /**
+     * Test method for {@link BinarySearchTree#toString()} should return empty string when tree is empty
+     */
+    @Test
+    void testToStringInOrderOnEmptyTree() {
+        String searchTreeString = searchTree.toString();
+        assertEquals("", searchTreeString);
+    }
+
+    /**
+     * Test method for {@link BinarySearchTree#toString()} should return inOrder string representation when tree is initialized
+     */
+    @Test
+    void testToStringInOrderOnInitializedTree() {
+        initializeSearchTree();
+        String searchTreeString = searchTree.toString();
+        assertEquals("12345", searchTreeString);
     }
 
     private void initializeSearchTree() {
